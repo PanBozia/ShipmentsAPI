38e0f830bb713f1038e5c51ae43adcdab8132e0b
LK-Herman <lkuczma@gmail.com>
2023-04-03 20:49:15 +0200

    frontv01

---
+++ shipments-vue/src/components/NavbarStart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="navbar">
+        <div class="navbar-line"></div>
+        <div class="navbar-content">
+            <div>
+                <router-link :to="{name: 'ShipmentsView'}">
+                    <p>BIURO</p>
+                </router-link>
+            </div>
+            <div>
+                    <router-link :to="{name:'ForwarderView'}">
+                    <p>MAGAZYN</p>
+                </router-link>
+            </div>
+            
+        </div>
+        <div class="navbar-line"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    // setup(){
+    //     const reloadPage = ()=>{
+    //         location.reload()
+    //     }
+    //     return { reloadPage}
+    // }
+
+}
+</script>
+
+
+
+<style scoped>
+.material-symbols-outlined{
+    font-size: 2.2vh;
+    padding: 0;
+    margin: 0;
+}
+
+</style>
--- shipments-vue/src/views/HomeView.vue
+++ shipments-vue/src/views/HomeView.vue
@@ -1,16 +1,23 @@
 <template>
-  <NavbarComponent />
+  <NavbarStart />
   <div class="frame">
-    <div style="display:flex; justify-content:center; border-bottom:solid 1px #fff; margin:30px 40px 0px 40px;">
-        <h1>NOTATNIK</h1>
-        
-    </div>
+    
     <div class="img-ctnr">
         <div></div>
-        <img class="test-image second" src="../assets/img/note01.jpg" alt="" srcset="">
-        <img class="test-image first" src="../assets/img/note02.jpg" alt="" srcset="">
-        <img class="test-image second" src="../assets/img/note04.jpg" alt="" srcset="">
-        <img class="test-image third" src="../assets/img/note03.jpg" alt="" srcset="">
+        <router-link :to="{name:'ShipmentsView'}" class="test-image first">
+            <img src="../assets/img/biuro.jpg">
+            <div>
+                <h1>Biuro</h1>
+                <h3>Awizacja wysyłek</h3>
+            </div>
+        </router-link>
+        <router-link :to="{name:'ShipmentsView'}" class="test-image first">
+            <img src="../assets/img/magazyn.jpg">
+            <div>
+                <h1>Magazyn</h1>
+                <h3>Obsługa wysyłek</h3>
+            </div>
+        </router-link>
         <div></div>
     </div>
 
@@ -18,9 +25,9 @@
 </template>
 
 <script>
-import NavbarComponent from '../components/NavbarComponent.vue'
+import NavbarStart from '../components/NavbarStart.vue'
 export default {
-    components: {NavbarComponent},
+    components: {NavbarStart},
     setup(){
         return{}
     }
@@ -33,26 +40,64 @@ export default {
     justify-items:center ; */
     display: flex;
     justify-content: center;
+    align-items: center;
+    height: 60vh;
+    
+}
+.img-ctnr img{
+    height: inherit;
+    overflow: hidden;
+    
 }
 .test-image{
-    height: 340px;
-    border-radius: 20px;
-    margin: 100px 10px;
-    box-shadow: 10px 10px 16px rgba(0,0,0,0.8);
+    display: grid;
+    grid-template-columns: auto auto;
+    gap: 3vh;
+    width: 50vh;
+    height: 24vh;
+    border-radius: 0vh;
+    margin: 10vh 3vh;
+    border: solid 0.3vh #ffffff;
+    justify-content: start;
+    align-items: center;
+    background: linear-gradient(rgb(56, 56, 56), rgb(38, 41, 42));
+}
+
+.test-image h1{
+    margin:0vh 0vh;
+    font-family: 'Bebas Neue', cursive;
+    background: -webkit-linear-gradient(#ffcc00, #fada27);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 6.0vh;
+    align-self: center;
+    padding: 0;
+    z-index: 2;
+}
+.test-image h3{
+    margin:0vh 0vh;
+    font-family: 'Bebas Neue', cursive;
+    background: -webkit-linear-gradient(#ffffff, #e6e6e6);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 3.0vh;
+    align-self: center;
+    padding: 0;
 }
 .test-image.first:hover{
-    transform:scale(2.0);
+    transform:scale(1.05);
     z-index: 2;
-    transition: ease-in-out 300ms;
+    transition: ease-in-out 200ms;
+    box-shadow: 1.6vh 1.6vh 1.8vh #00000060;
+    cursor: pointer;
 }
 .test-image.first{
-    transform: rotateZ(10deg) translateX(-40px);
-    transition: ease-in-out 300ms;
+    transition: ease-in-out 200ms;
 }
 .test-image.second:hover{
     transform:scale(2.0);
     z-index: 2;
-    transition: ease-in-out 300ms;
+    transition: ease-in-out 200ms;
 }
 .test-image.second{
     transform: rotateZ(-10deg) translateX(40px);
