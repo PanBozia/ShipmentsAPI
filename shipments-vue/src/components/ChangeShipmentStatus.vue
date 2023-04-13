ff2d5229c3d6ab5fe39b359b99a26f406bb14822
LK-Herman <lkuczma@gmail.com>
2023-04-13 21:14:00 +0200

    StatusChange

--- shipments-vue/src/components/ChangeShipmentStatus.vue
+++ shipments-vue/src/components/ChangeShipmentStatus.vue
@@ -1,44 +1,40 @@
 <template>
-  <div>
-        <form class="form-add" @submit.prevent="handleSubmit">
-            <div class="choose-order-container">
-                <div class="form-set"> 
-                    <div>
-                        <label class="form-labels">Status</label>
-                    </div>
-                    <div class="double-in">
-                        <select v-model="statusIdForm">
-                            <option :value="status.id" v-for="status in statuses" :key="status.id">
-                                {{status.name}}
-                            </option>
-                        </select>
-                    </div>
-               
-                </div>
-           </div>
-        </form>
+    <div>
+        <div class="status-ctnr" v-for="status in statuses" :key="status.id">
+            <StatusComponent v-model="status.name" :statusName="status.name" />
+        </div>    
     </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { onBeforeMount, ref } from 'vue'
 import changeStatus from '../js-components/changeStatus.js'
+import getStatuses from '../js-components/getStatuses.js'
+import StatusComponent from '../components/StatusComponent.vue'
 export default {
 props:['shipmentId'],
+components:{StatusComponent},
 setup(props){
     const url = 'https://localhost:44331/api/'
     const { change, error} = changeStatus(url)
+    const { loadStatuses, error:loadError, statuses} = getStatuses(url)
+    
     const statusIdForm = ref(1)
 
+    onBeforeMount(()=>{
+        loadStatuses()
+    })
+
     const handleSubmit = ()=>{
-        change(props.shipmentId, statusIdForm.value)
+        change(props.shipmentId, statusIdForm.value, statuses, loadError)
     }
 
 
     return {
         handleSubmit,
         error,
-        statusIdForm
+        statusIdForm,
+        statuses
     }
 }
 }
---
+++ shipments-vue/src/components/StatusComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="status-ctnr"> 
+    <div>
+        <p>
+            {{props.statusName}}
+        </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+    props:['statusName'],
+    setup(props) {
+     
+     return {props}
+    }
+}
+</script>
+<style>
+.status-ctnr{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 24vh;
+    margin: 0.2vh;
+    
+}
+.status-ctnr div{
+    padding: 0.4vh 3vh;
+    background-color: #25507c;
+}
+.status-ctnr div p{
+    margin: 0;
+    padding: 0;
+}
+</style>
